flow-matching training, fix actions scaling

wmp/modeling_vjepa2.py
@@ -193,7 +193,7 @@ class VJEPA2JointDiffuserRopeAttention(nn.Module):
             num_condition_frames - 1
         ) / self.config.tubelet_size
         action_position_ids_t = (
-            torch.arange(action_hidden_states.shape[1]) * (video_fps / action_fps)
+            torch.arange(action_hidden_states.shape[-2]) * (video_fps / action_fps)
             + final_observation_frame_position_id
         )
         # Use same position IDs for all dimensions. This strategy is used in Qwen2.5-Omni, page 4.
@@ -424,8 +424,9 @@ class VJEPA2JointDiffuser(nn.Module):
             nn.Mish(),
             nn.Linear(config.hidden_size, config.hidden_size * 3),
         )
+        self.flow_proj = nn.Linear(config.hidden_size, config.hidden_size)
+        self.flow_proj_actions = nn.Linear(config.hidden_size, action_dim)
 
-    @can_return_tuple
     def forward(
         self,
         video_latents: torch.Tensor,
@@ -436,13 +437,7 @@ class VJEPA2JointDiffuser(nn.Module):
         action_fps: float,
         num_condition_frames: int,
         head_mask: Optional[torch.Tensor] = None,
-        output_attentions: bool = False,
-        output_hidden_states: bool = False,
-        **kwargs,
-    ) -> BaseModelOutput:
-        all_hidden_states = () if output_hidden_states else None
-        all_self_attentions = () if output_attentions else None
-
+    ):
         # `video` is noisy latents
         video_hidden_states = video_latents
         video_hidden_states[
@@ -464,15 +459,15 @@ class VJEPA2JointDiffuser(nn.Module):
                 num_condition_frames,
                 None,
                 layer_head_mask,
-                output_attentions,
+                output_attentions=False,
             )
             video_hidden_states = layer_outputs[0]
             action_hidden_states = layer_outputs[1]
 
         video_hidden_states = self.layernorm(video_hidden_states)
+        action_hidden_states = self.layernorm(action_hidden_states)
 
-        return BaseModelOutput(
-            last_hidden_state=video_hidden_states,
-            hidden_states=all_hidden_states,
-            attentions=all_self_attentions,
+        return (
+            self.flow_proj(video_hidden_states),
+            self.flow_proj_actions(action_hidden_states),
         )

wmp/train_vjepa2_diffusion.py
@@ -1,40 +1,117 @@
-"""
-V-JEPA2
-"""
-
-import time
 from typing import cast
 
 import av
+from torch.utils.tensorboard import SummaryWriter
 import torch
 import torch.nn.functional as F
 from transformers import VJEPA2Model, VJEPA2VideoProcessor
 
-
-class SimultaneousDiffuser:
-    pass
+from wmp.modeling_vjepa2 import VJEPA2JointDiffuser
 
 
 def load_video(path):
     with av.open(path) as container:
-        return [frame.to_image() for frame in container.decode(video=0)]
+        return [frame.to_image() for frame in container.decode(video=0)]  # type: ignore
+
+
+# https://github.com/Wan-Video/Wan2.1/blob/main/wan/utils/fm_solvers.py
+def get_sampling_sigmas(sampling_steps, shift):
+    sigma = torch.linspace(1, 0, sampling_steps + 1)[:sampling_steps]
+    sigma = shift * sigma / (1 + (shift - 1) * sigma)
+
+    return sigma
+
+
+def logit_normal_distribution(x, mu, sigma):
+    logit_x = torch.log(x / (1 - x + 1e-8))
+    return (
+        1
+        / (sigma * ((2 * torch.pi) ** 0.5) * (x * (1 - x) + 1e-8))
+        * torch.exp(-0.5 * ((logit_x - mu) / sigma) ** 2)
+    )
 
 
 def main():
     device = "cpu"
     ckpt = "facebook/vjepa2-vitl-fpc64-256"
-    # model = VJEPA2Model.from_pretrained(ckpt).to(device)
+    original_model = VJEPA2Model.from_pretrained(ckpt).to(device)  # type: ignore
+    model = VJEPA2JointDiffuser(config=original_model.config, action_dim=2)
+    optim = torch.optim.AdamW(model.parameters(), lr=1e-4)
+
+    missing_keys, unexpected_keys = model.load_state_dict(
+        original_model.encoder.state_dict(), strict=False
+    )
+
+    if len(unexpected_keys) > 0:
+        print(unexpected_keys)
+    else:
+        print("No unexpected keys")
+
     processor = cast(VJEPA2VideoProcessor, VJEPA2VideoProcessor.from_pretrained(ckpt))
+
+    # overfit to a single example
 
     all_abs_actions = torch.load("./data/pusht_noise/train/abs_actions.pth")
 
+    # scale from [-200, 800] to [-1, 1]
+    all_abs_actions = (all_abs_actions - (-200)) / (800 - (-200)) * 2 - 1
+
     # load a video, 16x256x256x3 float32, and the corresponding actions
     obs = load_video("./data/pusht_noise/train/obses/episode_000.mp4")
-    actions = all_abs_actions[0][: len(obs)]
+    obs = obs[:8]
+
+    writer = SummaryWriter()
+
+    num_condition_frames = 2
+    action = (
+        all_abs_actions[0][num_condition_frames - 1 : len(obs)]
+        .to(torch.float32)
+        .unsqueeze(0)
+    )
     obs = processor(obs, return_tensors="pt").to(device)
     pv = obs["pixel_values_videos"]
 
-    print(pv.shape, actions.shape)
+    with torch.no_grad():
+        video_latents = original_model(
+            pixel_values_videos=pv, skip_predictor=True
+        ).last_hidden_state
+
+    sigmas = get_sampling_sigmas(sampling_steps=8, shift=1)
+    sigma_probs = logit_normal_distribution(sigmas, mu=0, sigma=1)
+
+    for i in range(1000):
+        sigma_index = torch.multinomial(sigma_probs, num_samples=1, replacement=False)
+        sigma = sigmas[sigma_index]
+        alpha = 1 - sigma
+
+        video_latents_noise = torch.randn_like(video_latents)
+        video_latents_noised = alpha * video_latents + sigma * video_latents_noise
+        action_noise = torch.randn_like(action)
+        action_noised = alpha * action + sigma * action_noise
+        video_latents_flow = video_latents_noise - video_latents
+        action_flow = action - action_noise
+        video_flow_pred, actions_flow_pred = model(
+            video_latents=video_latents_noised,
+            action=action_noised,
+            video_t=alpha,
+            action_t=alpha,
+            video_fps=1.0,
+            action_fps=1.0,
+            num_condition_frames=num_condition_frames,
+        )
+        video_flow_error = F.mse_loss(video_latents_flow, video_flow_pred)
+        action_flow_error = F.mse_loss(action_flow, actions_flow_pred)
+        loss = video_flow_error + action_flow_error
+        loss.backward()
+        optim.step()
+        optim.zero_grad()
+
+        writer.add_scalar("loss/total", loss.item(), i)
+        writer.add_scalar("loss/video", video_flow_error.item(), i)
+        writer.add_scalar("loss/actions", action_flow_error.item(), i)
+        writer.flush()
+
+        print(f"Step {i}: Loss = {loss.item()}")
 
 
 if __name__ == "__main__":