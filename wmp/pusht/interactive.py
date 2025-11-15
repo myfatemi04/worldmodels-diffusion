d0011d83fb23f4a107478d808421dce5013f020f
Michael Fatemi <myfatemi04@gmail.com>
2025-11-15 18:31:15 -0500

    create interactive PushT

---
+++ wmp/pusht/interactive.py
@@ -0,0 +1,75 @@
+from typing import cast
+
+import gym_pusht
+import gymnasium as gym
+import numpy as np
+import pygame
+
+env = gym.make(
+    "gym_pusht/PushT-v0",
+    render_mode="rgb_array",
+    visualization_width=512,
+    visualization_height=512,
+)
+
+
+observation, info = env.reset()
+
+image = env.render()
+
+# Display the image using pygame
+pygame.init()
+screen = pygame.display.set_mode((512, 512))
+pygame.display.set_caption("PushT Environment")
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    action = observation[:2]
+
+    # Handle key presses for actions
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP]:
+        action[1] -= 1.0
+    if keys[pygame.K_DOWN]:
+        action[1] += 1.0
+    if keys[pygame.K_LEFT]:
+        action[0] -= 1.0
+    if keys[pygame.K_RIGHT]:
+        action[0] += 1.0
+    if keys[pygame.K_q]:
+        running = False
+
+    observation, reward, terminated, truncated, info = env.step(action)
+    image = cast(np.ndarray, env.render())
+    image_chw = np.transpose(image, (1, 0, 2))
+
+    # Plot image
+    surf = pygame.surfarray.make_surface(image_chw)
+    screen.blit(surf, (0, 0))
+    pygame.display.flip()
+
+    # Display the reward
+    pygame.display.set_caption(f"Reward: {reward:.2f}")
+
+env.close()
+
+# agent_x, agent_y, block_x, block_y, block_angle = observation
+
+# block_ray_length = 50  # Example length of the ray
+
+# plt.imshow(image, origin="lower")
+# plt.scatter([agent_x], [agent_y], c="blue", label="Agent")
+# plt.gca().annotate(
+#     "",
+#     xy=(
+#         block_x + np.cos(block_angle) * block_ray_length,
+#         block_y + np.sin(block_angle) * block_ray_length,
+#     ),
+#     xytext=(block_x, block_y),
+#     arrowprops=dict(color="red", arrowstyle="->", lw=2),
+# )
+# plt.show()
---
+++ wmp/pusht/visualize_demo.py
@@ -0,0 +1,70 @@
+"""
+This test demonstrates the ability to interpret states from PushT.
+"""
+
+import pickle
+from typing import cast
+
+import av
+import gym_pusht
+import gymnasium as gym
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+from gym_pusht.envs.pusht import PushTEnv
+
+
+def load_demonstrations():
+    states = torch.load("data/pusht_noise/train/states.pth")
+    with open("data/pusht_noise/train/seq_lengths.pkl", "rb") as f:
+        seq_lengths = pickle.load(f)
+    abs_actions = torch.load("data/pusht_noise/train/abs_actions.pth")
+    rel_actions = torch.load("data/pusht_noise/train/rel_actions.pth")
+
+    return [
+        (
+            states[i][: seq_lengths[i]],
+            abs_actions[i][: seq_lengths[i]],
+            rel_actions[i][: seq_lengths[i]],
+        )
+        for i in range(len(states))
+    ]
+
+
+def render_demonstration(env: PushTEnv, states: torch.Tensor) -> list[np.ndarray]:
+    # Creates a video ndarray in [T, H, W, C] int8 format.
+
+    images = []
+
+    env.reset()
+    for state in states:
+        env._set_state(state.numpy())
+        images.append(env.render())
+
+    return images
+
+
+env = gym.make(
+    "gym_pusht/PushT-v0",
+    render_mode="rgb_array",
+    visualization_width=512,
+    visualization_height=512,
+)
+
+demos = load_demonstrations()
+states, abs_actions, rel_actions = demos[0]
+
+video = render_demonstration(env.env.env.env, states)  # type: ignore
+
+# Save video.
+container = av.open("pusht_demo.mp4", mode="w")
+stream = container.add_stream("libx264", rate=30)
+stream.width = 512
+stream.height = 512
+for frame in video:
+    frame = av.VideoFrame.from_ndarray(frame, format="rgb24")
+    for packet in stream.encode(frame):
+        container.mux(packet)
+for packet in stream.encode():
+    container.mux(packet)
+container.close()
