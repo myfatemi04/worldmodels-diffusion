c6778e82b4be729f504376c64d86e9db389352db
Michael Fatemi <myfatemi04@gmail.com>
2025-11-26 21:54:53 -0500

    add the cosmos stuff

--- wmp/pusht/train.py
+++ wmp/pusht/train.py
@@ -1,5 +1,7 @@
 # pip install --root-user-action ignore accelerate diffusers av loguru matplotlib gymnasium gym-pusht "pymunk<7"; apt install -y libgl1-mesa-glx htop zip unzip
 
+# torchrun --nproc_per_node=1 --master_port=12341 -m scripts.train --config=train.py -- experiment=predict2_video2world_training_2b_pusht_128
+
 import pickle
 
 import av
@@ -661,8 +663,8 @@ def test_tokenizers():
 def populate_sample_video_dir():
     import os
 
-    if not os.path.exists("sample_dataset/videos"):
-        os.makedirs("sample_dataset/videos")
+    os.makedirs("sample_dataset/videos", exist_ok=True)
+    os.makedirs("sample_dataset/metas", exist_ok=True)
 
     dataset = Dataset(
         demos=load_demonstrations(),
@@ -698,4 +700,278 @@ def populate_sample_video_dir():
     df.to_csv("sample_dataset/metadata.csv", index=False)
 
 
-populate_sample_video_dir()
+# export LD_LIBRARY_PATH=/root/cosmos-predict2.5/.venv/lib/python3.10/site-packages/nvidia/cuda_runtime/lib
+# torchrun --nproc_per_node=1 --master_port=12341 -m scripts.train --config=cosmos_predict_2_train.py -- experiment=predict2_video2world_training_2b_pusht_128
+
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import importlib
+from typing import Any, List
+
+import attrs
+
+from cosmos_predict2._src.imaginaire import config
+from cosmos_predict2._src.imaginaire.trainer import ImaginaireTrainer as Trainer
+from cosmos_predict2._src.imaginaire.utils.config_helper import (
+    import_all_modules_from_package,
+)
+from cosmos_predict2._src.predict2.configs.common.defaults.checkpoint import (
+    register_checkpoint,
+)
+from cosmos_predict2._src.predict2.configs.common.defaults.ckpt_type import (
+    register_ckpt_type,
+)
+from cosmos_predict2._src.predict2.configs.common.defaults.dataloader import (
+    register_training_and_val_data,
+)
+from cosmos_predict2._src.predict2.configs.common.defaults.ema import register_ema
+from cosmos_predict2._src.predict2.configs.common.defaults.optimizer import (
+    register_optimizer,
+)
+from cosmos_predict2._src.predict2.configs.common.defaults.scheduler import (
+    register_scheduler,
+)
+from cosmos_predict2._src.predict2.configs.common.defaults.tokenizer import (
+    register_tokenizer,
+)
+from cosmos_predict2._src.predict2.configs.video2world.defaults.callbacks import (
+    register_callbacks,
+)
+from cosmos_predict2._src.predict2.configs.video2world.defaults.conditioner import (
+    register_conditioner,
+)
+from cosmos_predict2._src.predict2.configs.video2world.defaults.model import (
+    register_model,
+)
+from cosmos_predict2._src.predict2.configs.video2world.defaults.net import register_net
+
+
+@attrs.define(slots=False)
+class Config(config.Config):
+    # default config groups that will be used unless overwritten
+    # see config groups in registry.py
+    defaults: List[Any] = attrs.field(
+        factory=lambda: [
+            "_self_",
+            {"data_train": "mock"},
+            {"data_val": "mock"},
+            {"optimizer": "fusedadamw"},
+            {"scheduler": "lambdalinear"},
+            {"model": "ddp"},
+            {"callbacks": "basic"},
+            {"net": None},
+            {"conditioner": "video_prediction_conditioner"},
+            {"ema": "power"},
+            {"tokenizer": "cosmos_tokenizer_causal_cv8x8x8_c16_res720_t121_it121_v1_0"},
+            {"checkpoint": "s3"},
+            {"ckpt_type": "dummy"},
+            # the list is with order, we need global experiment to be the last one
+            {"experiment": None},
+        ]
+    )
+
+
+from hydra.core.config_store import ConfigStore
+
+from cosmos_predict2._src.imaginaire.lazy_config import LazyCall as L
+from cosmos_predict2._src.imaginaire.utils.checkpoint_db import get_checkpoint_path
+from cosmos_predict2._src.predict2.datasets.local_datasets.dataset_video import (
+    VideoDataset,
+    get_generic_dataloader,
+    get_sampler,
+)
+from cosmos_predict2.config import MODEL_CHECKPOINTS, ModelKey
+
+DEFAULT_CHECKPOINT = MODEL_CHECKPOINTS[ModelKey(post_trained=False)]
+
+
+# GR1 dataset and dataloader
+example_video_dataset = L(VideoDataset)(
+    dataset_dir="/root/sample_videos",
+    num_frames=17,  # generate a bunch of videos
+    video_size=(128, 128),
+)
+
+# Create DataLoader with distributed sampler
+dataloader_train = L(get_generic_dataloader)(
+    dataset=example_video_dataset,
+    sampler=L(get_sampler)(dataset=example_video_dataset),
+    batch_size=1,
+    drop_last=True,
+    num_workers=4,
+    pin_memory=True,
+)
+
+# Video2World post-training configuration for 2B model
+# torchrun --nproc_per_node=1 --master_port=12341 -m scripts.train --config=cosmos_predict2/_src/predict2/configs/video2world/config.py -- experiment=predict2_video2world_training_2b_pusht_128
+predict2_video2world_training_2b_pusht_128 = dict(
+    defaults=[
+        f"/experiment/{DEFAULT_CHECKPOINT.experiment}",
+        {"override /data_train": "mock"},
+        {"override /data_val": "mock"},
+        "_self_",
+    ],
+    dataloader_train=dataloader_train,
+    checkpoint=dict(
+        save_iter=200,
+        # pyrefly: ignore  # missing-attribute
+        load_path=get_checkpoint_path(DEFAULT_CHECKPOINT.s3.uri),
+        load_from_object_store=dict(
+            enabled=False,
+        ),
+        save_to_object_store=dict(
+            enabled=False,
+        ),
+    ),
+    job=dict(
+        project="cosmos_predict_v2p5",
+        group="video2world",
+        name="2b_pusht_128",
+    ),
+    optimizer=dict(
+        lr=2 ** (-14.5),
+        weight_decay=0.001,
+    ),
+    scheduler=dict(
+        f_max=[0.5],
+        f_min=[0.2],
+        warm_up_steps=[1_000],
+        cycle_lengths=[100000],
+    ),
+    trainer=dict(
+        logging_iter=100,
+        max_iter=1000,
+        callbacks=dict(
+            heart_beat=dict(
+                save_s3=False,
+            ),
+            iter_speed=dict(
+                hit_thres=100,
+                save_s3=False,
+            ),
+            device_monitor=dict(
+                save_s3=False,
+            ),
+            every_n_sample_reg=dict(
+                every_n=200,
+                save_s3=False,
+            ),
+            every_n_sample_ema=dict(
+                every_n=200,
+                save_s3=False,
+            ),
+            wandb=dict(
+                save_s3=False,
+            ),
+            wandb_10x=dict(
+                save_s3=False,
+            ),
+            dataloader_speed=dict(
+                save_s3=False,
+            ),
+        ),
+    ),
+    model_parallel=dict(
+        context_parallel_size=1,
+    ),
+)
+
+
+def make_config() -> Config:
+    c = Config(
+        model=None,
+        optimizer=None,
+        scheduler=None,
+        dataloader_train=None,
+        dataloader_val=None,
+    )
+
+    # Specifying values through instances of attrs
+    c.job.project = "cosmos_diffusion_v2"
+    c.job.group = "debug"
+    c.job.name = "delete_${now:%Y-%m-%d}_${now:%H-%M-%S}"
+
+    c.trainer.type = Trainer
+    c.trainer.straggler_detection.enabled = False
+    c.trainer.max_iter = 400_000
+    c.trainer.logging_iter = 10
+    c.trainer.validation_iter = 100
+    c.trainer.run_validation = False
+    c.trainer.callbacks = None
+
+    # Call this function to register config groups for advanced overriding. the order follows the default config groups
+    register_training_and_val_data()
+    register_optimizer()
+    register_scheduler()
+    register_model()
+    register_callbacks()
+    register_net()
+    register_conditioner()
+    register_ema()
+    register_tokenizer()
+    register_checkpoint()
+    register_ckpt_type()
+
+    # experiment config are defined in the experiment folder
+    # call import_all_modules_from_package to register them
+    import_all_modules_from_package(
+        "cosmos_predict2._src.predict2.configs.video2world.experiment", reload=True
+    )
+    try:
+        if importlib.util.find_spec("cosmos_predict2.experiments.internal") is not None:
+            import_all_modules_from_package(
+                "cosmos_predict2.experiments.internal", reload=True
+            )
+    except ModuleNotFoundError:
+        pass  # Module or parent package doesn't exist
+    import_all_modules_from_package("cosmos_predict2.experiments", reload=True)
+
+    cs = ConfigStore.instance()
+
+    for _item in [
+        predict2_video2world_training_2b_pusht_128,
+    ]:
+        # Get the experiment name from the global variable
+        experiment_name = [
+            name.lower() for name, value in globals().items() if value is _item
+        ][0]
+        cs.store(
+            group="experiment",
+            package="_global_",
+            name=experiment_name,
+            node=_item,
+        )
+
+    return c
+
+
+# populate_sample_video_dir()
